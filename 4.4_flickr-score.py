1ffe29eb8e737594c5afee40452f05426f648f05
satyam9753 <satyam.anand9753@gmail.com>
2019-11-13 08:49:31 +0530

    small changes left

--- 4.4_flickr-score.py
+++ 4.4_flickr-score.py
@@ -2,109 +2,136 @@ import numpy as np
 import os
 import cv2
 import pandas as pd
-import keras
-from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Dropout
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
-from keras.layers import Activation
-from sklearn.datasets import load_sample_image
-from sklearn.feature_extraction import image
 import re2
+import statistics
 
-############################################################################################
+#import keras
+#from keras.models import Sequential
+#from keras.layers.core import Flatten, Dense, Dropout
+#from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+#from keras.layers import Activation
+#from sklearn.datasets import load_sample_image
+#from sklearn.feature_extraction import image
+
 
 
 DATADIR = "/home/satyam/Desktop/personal/flickr"
 
-# while true:  
-#   path = os.path.join(DATADIR,faves_score) 
-#    for img in os.listdir(path):  
-#        img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)
-#        plt.imshow(img_array, cmap='gray')
+class Dataset():
+
+    j=1
+    faves_score = list()
+
+    for img in os.listdir(DATADIR):
+
+        img_path = DATADIR + '/' + os.listdir(DATADIR)[img]
+        image = cv2.imread(img_path)
+
+        for file_name in img_path:
+            
+            faves = re2.findall("flickr_"+'j' + "_(\d+).jpg", file_name)
+            if not faves: 
+                continue
+
+            j++
+            faves_score.append(float(faves[0]))
 
 
-i=1
-for file_name in DATADIR:
+
+    median = statistics.median(faves_score)
+
+
+
+class Resize_images():
+ 
+    for img in os.listdir(DATADIR): 
+
+        img_path = DATADIR + '/' + os.listdir(DATADIR)[img]
+        image = cv2.imread(img_path)
+        resultant_image = cv2.resize(img, dsize=(720, 1280), interpolation=cv2.INTER_CUBIC)
+
+
+
+class FFS_label():
+    for i in (faves_score):
+
+    	if faves_score[i] > median:
+    		new_img_path = DATADIR + '/'+"H" + os.listdir(DATADIR)[i]                                              
+
+
+    	else:
+    		new_img_path = DATADIR + '/'+"L" + os.listdir(DATADIR)[i]
+
+
+class create_patch():
     
-    res = re2.findall("flickr_"+'i' + "_(\d+).jpg", file_name)
-    if not res: 
-        continue
+    for img in os.listdir(DATADIR):
 
-    i++
-    faves_score = res[0]
+        img_patch = cv2.imread(new_img_path)
+        patches = image.extract_patches_2d(img_patch, (224, 224), max_patches = 1)
 
-
-
-############################################################################################
-
-# RESISIZING THE IMAGE
-
-img = cv2.imread('your_image.jpg')
-res = cv2.resize(img, dsize=(720, 1280), interpolation=cv2.INTER_CUBIC)
-
-#############################################################################################
-
-median = np.median(faves)
-
-for i in (len):
-
-	if FFS> median:
-		quality = "high"
-
-	else:
-		quality = "low"
-
-	df['QUALITY'] = quality    #adding extra column
+        ##STORE PATCHES IN DIFFERENT DIRECTORY
 
 
-##############################################################################################
+class VGG_19():
 
-img_patch = load_sample_image("image to be patched")
-patches = image.extract_patches_2d(img_patch, (224, 224))
+        HEIGHT = 224
+        WIDTH = 224
+
+        base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(HEIGHT, WIDTH, 3))
 
 
-##### VGG-19 style ######
 
 
-def VGG_19(weights_path=None):
-    model = Sequential()
-    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-    
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+        def build_finetune_model(base_model, dropout, fc_layers, num_classes):
+            for layer in base_model.layers:
+                layer.trainable = False
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+            x = base_model.output
+            x = Flatten()(x)
+            for fc in fc_layers:
+                # New FC layer, random init
+                x = Dense(fc, activation='relu')(x) 
+                x = Dropout(dropout)(x)
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+            # New softmax layer
+            predictions = Dense(num_classes, activation='softmax')(x) 
+            
+            finetune_model = Model(inputs=base_model.input, outputs=predictions)
 
-    model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.3))
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.3))
-    model.add(Activation('softmax'))
+            return finetune_model
+
+        class_list = ["Original","Tampered"]
+        FC_LAYERS = [1024, 1024]
+        NUM_EPOCHS = 10
+        BATCH_SIZE = 4
+        dropout = 0.5
+
+        finetune_model = build_finetune_model(base_model, dropout=dropout, fc_layers=FC_LAYERS, num_classes=len(class_list))
+
+        # directory = './drive/My Drive/newdata2'
+
+        directory = '________________' ##DIRECTORY WHERE PATCHES ARE STORED
+
+        train_generator,valid_generator,test_generator=datagen.getGenerators(directory,train_batch_size=8,valid_batch_size=1,test_batch_size=1,train_ratio=0.6,valid_ratio=0.2)
+
+        # train_generator=DataGenerator2(directory, batch_size=BATCH_SIZE)
+
+
+        adam = Adam(lr=0.00001)
+        finetune_model.compile(adam, loss='binary_crossentropy', metrics=['accuracy'])
+
+        filepath="./checkpoints/" + "MobileNetV2" + "_model_weights2.h5"
+        if not os.path.exists("./checkpoints"):
+            os.makedirs("./checkpoints")
+
+        checkpoint = ModelCheckpoint(filepath, monitor="val_acc", verbose=1, save_best_only=True)
+        callbacks_list = [checkpoint]
+
+        history = finetune_model.fit_generator(train_generator, epochs=NUM_EPOCHS, workers=8,
+                                               shuffle=True, callbacks=callbacks_list,validation_data=valid_generator,validation_freq=5,use_multiprocessing=True)
+
+        # history = base_model.predict_generator(train_generator, workers=8)
+        print(finetune_model.evaluate_generator(test_generator))
 
 
